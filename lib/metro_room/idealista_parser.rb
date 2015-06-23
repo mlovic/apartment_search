691d09d13f28af267b9fa80273e7e8c173ba4006
Marko Lovic <markolovic@hotmail.com>
2015-06-23 18:40:14 +0200

    Clean up IdealistaParser error handling

--- lib/metro_room/idealista_parser.rb
+++ lib/metro_room/idealista_parser.rb
@@ -13,26 +13,24 @@ class IdealistaParser
   def self.get_listings(json)
     @obj = JSON.parse(json)
     properties = []
-    begin
-      @obj["elementList"].each do |prop|
-        prop = Property.new(prop["latitude"], prop["longitude"], prop["price"], prop["address"], prop["url"])
-        properties << prop
-      end
-      $log.info "#{properties.size} properties parsed: #{properties.first.address}."
-      return properties
-    rescue Exception => e  
-      $log.error "obj: #{@obj.inspect}"
-      if @obj[:fault][:faultstring] == "Spike arrest violation. Allowed rate : 1ps"
-        puts "ERR: spike arrest violation"
-        #TODO use test_data, write exception type
-      else
-        puts e.message  
-        puts e.backtrace.inspect  
-      end
-      #puts "Error parsing response body:\n\n"
-      #pp @obj
-      exit
+    @obj["elementList"].each do |prop|
+      prop = Property.new(prop["latitude"], prop["longitude"], prop["price"], prop["address"], prop["url"])
+      properties << prop
     end
+    $log.info "#{properties.size} properties parsed: #{properties.first.address}."
+    return properties
+  rescue StandardError => e  
+    puts e.message  
+    puts e.backtrace.inspect  
+    puts @obj.class.name
+    $log.error "error: #{e.message}"
+    $log.error "backtrace: #{e.backtrace.inspect}"
+    $log.error "obj: #{@obj.inspect}"
+    #if @obj[:fault][:faultstring] == "Spike arrest violation. Allowed rate : 1ps"
+      #puts "ERR: spike arrest violation"
+      ##TODO use test_data, write exception type
+      #puts e.backtrace.inspect  
+    #end
   end
 
 end
