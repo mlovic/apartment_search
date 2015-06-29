93c18c818f771eea9288e0a87b0ded3f440fa829
Marko Lovic <markolovic@hotmail.com>
2015-06-30 01:14:29 +0200

    Add spec helpers module

---
+++ spec/helpers.rb
@@ -0,0 +1,24 @@
+module Helpers
+
+  def default_configure
+    MetroRoom.configure do |config|
+      config.db_host      = 'localhost'
+      config.db_user      = 'root'
+      config.db_password  = 'mysql05'
+      config.db_name      = 'apartment_search'
+
+      config.api_key      = 'EzWE6qNgjSFTEs4plajIvMzmrm5DOset'
+
+      config.logger_stdout = true
+      config.logger_level  = 'DEBUG'
+    end
+  end
+
+  def init_db
+    metro_db = MetroDB.new(MetroRoom.configuration.db_host,
+                           MetroRoom.configuration.db_user,
+                           MetroRoom.configuration.db_password,
+                           MetroRoom.configuration.db_name)
+  end
+
+end
